987f71b7f20a8302869cce6483abfe04bf9141ce
Leslie Osorio <lesosorio17@gmail.com>
2018-11-08 19:36:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,10 +8,10 @@ num = [25,7,14]
 def sort_array_desc(num)
   num.sort do |b,a|
     b <=> a
-  end 
+  end
 
 animals = ["dogs","cat","Horses"]
 def sort_array_char_count(animals)
   animals.sort do |a,b|.length
-    a <=> b 
+    a <=> b
   end 
