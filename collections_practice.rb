f22a7ccbb197990df19ff23ae8e94812ad672138
Leslie Osorio <lesosorio17@gmail.com>
2018-11-08 20:55:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,7 +56,7 @@ end
 def add_s(array)
   new_array = []
   array.each_with_index do |word,index|
-    if index == 2
+    if index == 1
       new_array << word
     else
       new_array << word.concat("s")
