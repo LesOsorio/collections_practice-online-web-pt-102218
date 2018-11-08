5570e76ed36cd0c8ddaf12d8db876156667738ee
Leslie Osorio <lesosorio17@gmail.com>
2018-11-08 19:45:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,8 @@
 num = [25,7,1]
 def sort_array_asc(num)
   num.sort do |a,b|
-a <=> b
+    a <=> b
+  end
 end
 
 num = [25,7,14]
@@ -9,13 +10,14 @@ def sort_array_desc(num)
   num.sort do |b,a|
     b <=> a
   end
+end
 
 animals = ["dogs","cat","Horses"]
 def sort_array_char_count(animals)
   animals.sort do |a,b|.length
     a <=> b
-  end 
+  end
+end 
 
 names = ["blake","ashley","scott"]
 def swap_elements_from_to(names)
-  
